4fe0fa14641e1a9513f644868cee35ae25a297be
Donizetti Roberto <donizettibatista1@gmail.com>
2026-04-21 23:12:49 -0300

    feature: create scraper

---
+++ scraper.py
@@ -0,0 +1,31 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+import selenium
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+URL = os.getenv("URL")
+news= []
+
+options = Options()
+options.add_argument("--headless=new")
+
+driver = webdriver.Chrome(options = options)
+driver.get(URL)
+elements = driver.find_elements(By.XPATH,"//div[contains(@class,'_evt')]/h2//a")
+
+
+if __name__=="__main__":
+    for element in elements[:5]:
+        titulo = element.text
+        link = element.get_attribute("href")
+        news.append({
+            "titulo": element.text,
+            "subtitulo": "(IMPLEMENTAR)",
+            "descricao": "(IMPLEMENTAR)",
+            "link": link
+        })
