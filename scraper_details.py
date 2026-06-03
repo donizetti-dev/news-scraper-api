6b949fe677596c4fdb39853f893b4e6bc9ac85cd
Donizetti Roberto <donizettibatista1@gmail.com>
2026-06-03 15:09:48 -0300

    refactor: add log in scraper_listing

--- scraper_details.py
+++ scraper_details.py
@@ -12,7 +12,9 @@ from logger_config import logger
 
 def get_detail(driver):
     caption = driver.find_elements(By.XPATH,"//h2[contains(@class,'content-head__subtitle')]")
+
     description = driver.find_elements(By.XPATH,"//h2[contains(@class,'content-head__subtitle')]//ancestor::div[contains(@class,'mc-article-header')]//following::div[contains(@class,'mc-article-body')]//p[contains(@class,' content-text__container ')][1]")
+
     title = driver.find_elements(By.XPATH,"//main[contains(@class,'mc-body theme')]//h1[contains(@class,'content-head__title')]")
 
 
@@ -24,18 +26,26 @@ def get_detail(driver):
 
 def main():
     logger.info(f'{"="*30}{__name__}{"="*30}')
-    exit
+
+    logger.info(f'Verifica linhas pendentes para busca de detalhes')
     process = query_pending()
+
+    logger.info(f'Identificado {len(process)} processos pendentes')
 
     for news in process:
         driver = get_driver(news['link'])
 
-        dict_details = (get_detail(driver))
+        dict_details = get_detail(driver)
+        logger.info(f'Identificado detalhes para notícia {dict_details['titulo']}')
 
-
+        logger.info(f'Atualiza id {news['id']} com informações extraídas')
         update_details(news['id'],dict_details)
 
+        logger.info(f'Finaliza driver')
         driver.quit()
+
+    logger.info(f'Fim da execução')
+
 
 if __name__=='__main__':
     main()
--- scraper_listing.py
+++ scraper_listing.py
@@ -1,11 +1,10 @@
 import sys
 import os
-root = os.path.abspath(os.path.join(os.path.dirname(__file__), '../..'))
-sys.path.append(root)
 
 from dotenv import load_dotenv
 from database import insert_news
 from browser import get_driver
+from logger_config import logger
 
 load_dotenv()
 
@@ -21,37 +20,49 @@ URL = os.getenv("URL")
 news= []
 
 def load_page(driver):
+    logger.info(f'Iniciando função')
+
     count_loop = 30
+    logger.info(f'Inicia loop para carregar página até 30 casos')
     for i in range(count_loop):
         
         qtd_news = len(scraper_news(driver))
 
         if qtd_news>=30:
             #30 notícias carregadas, seguindo processamento
+            logger.info(f'30 notícias carregadas, seguindo processamento')
             break
 
-
+        logger.info(f'Aguardando carregamento {i}\{count_loop} notícias carregadas: {qtd_news}')    
         driver.execute_script("document.querySelector('#feed-placeholder > div > div > div.load-more.gui-color-primary-bg > a').click()")
 
         sleep(0.5)
 
 
 def scraper_news(driver) -> list:
-
+    logger.info(f'Buscando notícias')
     items = driver.find_elements(By.XPATH,"//div[contains(@class,'_evt')]/h2//a")
 
     return items
 
 
 def main():
+    logger.info(f'{"="*30}{__name__}{"="*30}')
+    
+    logger.info(f'Inicia navegador')
     driver = get_driver(URL)
 
+    logger.info(f'Chama função para carregar página')
     load_page(driver)
 
+    logger.info(f'Chama função para busca de notícias')
     elements = scraper_news(driver)
+
+    logger.info(f'Loop sobre notícias para insert de links no banco')
     for element in elements[:30]:
 
         link = element.get_attribute("href")
+        
         news.append({
             "titulo": "PENDENTE",
             "subtitulo": "PENDENTE",
